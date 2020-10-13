cdb4dd44855c495e81b0241c39eecb3612823cf4
Mitchell Nwakusor <udemenwak@gmail.com>
2020-10-13 22:38:58 +0100

    initial commit

---
+++ Assets/Scripts/rotateObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateObject : MonoBehaviour
+{
+    public GameObject obj;
+    public float rotationSpeed = 0.5f;
+    public bool rotate = false;
+    void Update()
+    {
+             if(rotate){
+                  obj.transform.Rotate(0, rotationSpeed, 0);
+             }
+             else{
+                  obj.transform.Rotate(0, 0, 0);
+                  obj.transform.rotation = Quaternion.identity;
+             }
+    }
+
+    public void play(){
+          rotate = true;
+    }
+
+    public void stop(){
+          rotate = false;
+    }
+}
